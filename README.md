cedcde5e662abba3c57ff3052a38f0259fb83087
alexchavez1980 <alexchavez1980@gmail.com>
2022-10-29 08:44:24 -0300

    dc10_con_dag

---
+++ README.md
@@ -0,0 +1,36 @@
+# Seminario de Tópicos Avanzados en Datos Complejos
+forked from https://github.com/arjones/bigdata-workshop-es
+
+
+Docente:   
+Pedro Ferrari | pedro@muttdata.ai  
+Juan Martín Pampliega | jpamplie@itba.edu.ar  
+
+Estudiantes:    
+
+Guillermo Lencina | glencina@itba.edu.ar    
+Nicolas Arosteguy | narosteguy@itba.edu.ar    
+Alexander Chavez | achavezmontano@itba.edu.ar   
+
+
+## Pasos para instalar todo y ver avances:
+
+1. Abrir _Docker Desktop_ y tener la interfaz para ver los resultados. Acá no hay que hacer nada.
+2. Abrir WSL, luego la consola de Ubuntu.
+3. Ejecutar/Descargar todo el proyecto: git clone https://github.com/alexchavez1980/nga5.git
+4. Ingresar: cd nga
+5. Ejecutar: docker-compose -f <archivo.yml> up -d. Ejemplo: docker-compose -f dc10.yml up -d
+
+* El docker-compose es un archivo yaml/yml para crear todos los containers necesarios y a la vez.
+* Para borrar todos los containers, incluso las redes: docker compose down
+* Para conocer las redes: docker network ls. 
+* Para conocer qué IPs fue asignadas a cada uno de los containers dentro de la red: 
+    docker network inspect <nombre de la red>
+
+Sitios donde miré tema Dockers: 
+
+    * https://www.youtube.com/c/PeladoNerd  
+    * https://www.youtube.com/c/HolaMundoDev  
+    * https://www.youtube.com/c/NetworkChuck
+    * Postgres + PGAdmin : https://www.youtube.com/watch?v=uKlRp6CqpDg  
+
---
+++ airflow/dags/nga_hello_world_dag.py
@@ -0,0 +1,60 @@
+#datetime
+from datetime import timedelta, datetime
+
+# The DAG object
+from airflow import DAG
+
+# Operators
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+# initializing the default arguments
+default_args = {
+		'owner': 'Ranga',
+		'start_date': datetime(2022, 3, 4),
+		'retries': 3,
+		'retry_delay': timedelta(minutes=5)
+}
+
+# Instantiate a DAG object
+hello_world_dag = DAG('hello_world_NGA_dag',
+		default_args=default_args,
+		description='Hello World NGA DAG',
+		schedule_interval='* * * * *', 
+		catchup=False,
+		tags=['example, helloworld']
+)
+
+# python callable function
+def print_hello():
+		return 'Hello World NGA!'
+
+# Creating first task
+start_task = DummyOperator(task_id='start_task', dag=hello_world_dag)
+
+# Creating second task
+hello_world_task = PythonOperator(task_id='hello_world_task', python_callable=print_hello, dag=hello_world_dag)
+
+# Creating third task
+end_task = DummyOperator(task_id='end_task', dag=hello_world_dag)
+
+# Set the order of execution of tasks. 
+start_task >> hello_world_task >> end_task
+# --------------------------------------------------------------------------
+# Pruebas inconexas. Abajo hago el mismo test de siempre:
+# --------------------------------------------------------------------------
+#install yfinance
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import datetime
+pd.options.display.float_format = '{:.4f}'.format # Flotantes de 4 decimales.
+
+ds_BTC = yf.download('BTC-USD', period = '1mo', interval = '60m')
+#ds_BTC.describe()
+
+ds_BTC['DateTime'] = ds_BTC.index # Importante: Incluyo la cronología en una columna
+ds_BTC = ds_BTC[['DateTime','Open','High','Low','Close','Adj Close','Volume']]
+#ds_BTC.head()
+
+ds_BTC.to_csv(r'./ds_BTC_PERSISTENCIA_AIRFLOW.csv') # , sep='\t', encoding='utf-8', header='true'
